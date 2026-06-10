fb29ce86795ec31ec22a0b604c91e5b3b5b215ee
whitegum <114851839+AKCASAKIZ@users.noreply.github.com>
2026-06-10 11:28:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 API_KEY = os.getenv("GEMINI_API_KEY")
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+GITHUB_TOKEN = os.getenv("EZEL_TOKEN")
 GITHUB_REPO = os.getenv("GITHUB_REPO")
 
 if not API_KEY:
